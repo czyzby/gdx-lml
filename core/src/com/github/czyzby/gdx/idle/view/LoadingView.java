5466955078735708256fe42d449a7b1e547accd1
MJ <john.hervicc@gmail.com>
2015-05-12 23:29:16 +0200

    Updated to 0.6.1.6.0 Autumn MVC version.

--- core/src/com/github/czyzby/gdx/idle/view/LoadingView.java
+++ core/src/com/github/czyzby/gdx/idle/view/LoadingView.java
@@ -5,7 +5,6 @@ import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.ProgressBar;
 import com.github.czyzby.autumn.annotation.field.Inject;
 import com.github.czyzby.autumn.mvc.component.asset.AssetService;
-import com.github.czyzby.autumn.mvc.component.ui.InterfaceService;
 import com.github.czyzby.autumn.mvc.component.ui.controller.ViewRenderer;
 import com.github.czyzby.autumn.mvc.stereotype.View;
 import com.github.czyzby.autumn.mvc.stereotype.ViewActor;
@@ -14,8 +13,6 @@ import com.github.czyzby.autumn.mvc.stereotype.ViewActor;
 public class LoadingView implements ViewRenderer {
 	@Inject
 	private AssetService assetService;
-	@Inject
-	private InterfaceService interfaceService;
 
 	@ViewActor("loadingBar")
 	private ProgressBar loadingBar;
@@ -25,9 +22,7 @@ public class LoadingView implements ViewRenderer {
 
 	@Override
 	public void render(final Stage stage, final float delta) {
-		if (assetService.update()) {
-			interfaceService.show(MenuView.class);
-		}
+		assetService.update();
 		updateProgress();
 		stage.act(delta);
 		stage.draw();
@@ -35,7 +30,7 @@ public class LoadingView implements ViewRenderer {
 
 	private void updateProgress() {
 		final float currentProgress = assetService.getLoadingProgress();
-		loadingBar.setValue(progress);
+		loadingBar.setValue(currentProgress);
 		final int progressPercent = (int) (currentProgress * 100f);
 		if (progressPercent != progress) {
 			progress = progressPercent;
