1030839a07d11098fcbf6cb538a64459fd7ec02b
MJ <john.hervicc@gmail.com>
2015-05-12 23:07:01 +0200

    Updated to 0.6.1.6.0 LML version.

--- html/src/com/github/czyzby/tests/client/HtmlLauncher.java
+++ html/src/com/github/czyzby/tests/client/HtmlLauncher.java
@@ -3,6 +3,7 @@ package com.github.czyzby.tests.client;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.backends.gwt.GwtApplication;
 import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.badlogic.gwtref.client.ReflectionCache;
 import com.github.czyzby.tests.Main;
 
 public class HtmlLauncher extends GwtApplication {
@@ -13,6 +14,23 @@ public class HtmlLauncher extends GwtApplication {
 
 	@Override
 	public ApplicationListener getApplicationListener() {
+		logAllProperties(getCache());
 		return new Main();
 	}
+
+	private native void logAllProperties(Object object)/*-{
+		var prototype=object.__proto__;
+		for( var property in prototype){
+		console.log(prototype[property]);
+		}
+		console.log(prototype);
+	}-*/;
+
+	private native void log(Object message)/*-{
+		console.log(message);
+	}-*/;
+
+	private native ReflectionCache getCache() /*-{
+		return @com.badlogic.gwtref.client.ReflectionCache::instance;
+	}-*/;
 }
