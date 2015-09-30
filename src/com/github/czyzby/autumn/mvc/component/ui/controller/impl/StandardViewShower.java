7012416fbbf44188f8c9c3a5549011bdfafe8f36
MJ <john.hervicc@gmail.com>
2015-09-30 10:54:32 +0200

    Removed deprecated LML tooltip handling.

--- src/com/github/czyzby/autumn/mvc/component/ui/controller/impl/StandardViewShower.java
+++ src/com/github/czyzby/autumn/mvc/component/ui/controller/impl/StandardViewShower.java
@@ -5,17 +5,16 @@ import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Dialog;
 import com.github.czyzby.autumn.mvc.component.ui.controller.ViewShower;
-import com.github.czyzby.lml.gdx.widget.reflected.Tooltip;
 
-/** Shows and hides the screen by passing received action to the stage without any modifications. Removes tooltips and
- * dialogs before showing.
+/** Shows and hides the screen by passing received action to the stage without any modifications. Removes dialogs before
+ * showing.
  *
  * @author MJ */
 public class StandardViewShower implements ViewShower {
     @Override
     public void show(final Stage stage, final Action action) {
         for (final Actor actor : stage.getActors()) {
-            if (actor instanceof Dialog || actor instanceof Tooltip) {
+            if (actor instanceof Dialog) {
                 actor.remove();
             }
         }
