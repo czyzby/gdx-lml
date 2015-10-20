845bddf6261be5410fc82f9c3cfbdce3d0cb5965
MJ <john.hervicc@gmail.com>
2015-10-20 19:17:15 +0200

    Project creation.

---
+++ README.md
@@ -0,0 +1,3 @@
+#Vis UI parser for LML templates.
+
+See [gdx-lml](http://github.com/czyzby/gdx-lml). Instead of building your parser with `Lml` class (or `LmlParserBuilder`), use `VisLml` (or `VisLmlParserBuilder`).
---
+++ src/com/github/czyzby/lml/vis/parser/impl/VisLmlSyntax.java
@@ -0,0 +1,40 @@
+package com.github.czyzby.lml.vis.parser.impl;
+
+import com.github.czyzby.lml.parser.impl.DefaultLmlSyntax;
+import com.github.czyzby.lml.vis.parser.impl.attribute.textbutton.TextButtonFocusBorderEnabledLmlAttribute;
+import com.github.czyzby.lml.vis.parser.impl.tag.provider.VisTextButtonLmlTagProvider;
+
+/** Replaces regular Scene2D actor tags with Vis UI widgets. Supports the same core syntax (operators).
+ *
+ * @author MJ */
+public class VisLmlSyntax extends DefaultLmlSyntax {
+    @Override
+    protected void registerTags() {
+        registerVisActorTags();
+        registerMacroTags();
+    }
+
+    /** Registers all Vis UI actors' tags. */
+    protected void registerVisActorTags() {
+        addTagProvider(new VisTextButtonLmlTagProvider(), "textButton", "visTextButton", "a");
+        // TODO register other Vis tags
+    }
+
+    @Override
+    protected void registerAttributes() {
+        super.registerAttributes();
+        registerVisAttributes();
+    }
+
+    /** Registers attributes of Vis UI-specific actors. */
+    protected void registerVisAttributes() {
+        // TODO register attributes
+    }
+
+    @Override
+    protected void registerButtonAttributes() {
+        super.registerButtonAttributes();
+        // VisTextButton:
+        addAttributeProcessor(new TextButtonFocusBorderEnabledLmlAttribute(), "focusBorder", "focusBorderEnabled");
+    }
+}
