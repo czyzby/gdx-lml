5c0132b82c3d4631d612bef89085906ee279f232
MJ <john.hervicc@gmail.com>
2015-11-24 03:28:44 +0100

    Android import fix.

--- simple/android/src/com/github/czyzby/android/AndroidLauncher.java
+++ simple/android/src/com/github/czyzby/android/AndroidLauncher.java
@@ -1,5 +1,7 @@
 package com.github.czyzby.android;
 
+import android.os.Bundle;
+
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import com.github.czyzby.Configuration;
