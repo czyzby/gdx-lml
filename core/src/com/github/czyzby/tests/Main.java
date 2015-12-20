0b55130528802a1f2622e2b024f172f384523d4a
MJ <john.hervicc@gmail.com>
2015-12-20 19:34:43 +0100

    Added argument macro example.

--- core/src/com/github/czyzby/tests/Main.java
+++ core/src/com/github/czyzby/tests/Main.java
@@ -48,9 +48,9 @@ public class Main extends AbstractApplicationListener {
             // Syntax:
             "i18n", "preferences", "arguments", "actions",
             // Macros:
-            "actorMacro", "anyNotNull", "argumentReplace", "assign", "calculate", "comment", "conditional", "evaluate",
-            "exception", "forEach", "import", "loop", "logger", "meta", "nestedForEach", "newAttribute", "newTag",
-            "nullCheck", "tableColumn", "tableRow", "while",
+            "actorMacro", "anyNotNull", "argument", "argumentReplace", "assign", "calculate", "comment", "conditional",
+            "evaluate", "exception", "forEach", "import", "loop", "logger", "meta", "nestedForEach", "newAttribute",
+            "newTag", "nullCheck", "tableColumn", "tableRow", "while",
             // Custom elements:
             "customAttribute", "customMacro", "customTag",
             // Vis unique tags:
