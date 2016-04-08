36f50faeb1fd982c338a3b997749311b91429703
MJ <john.hervicc@gmail.com>
2016-04-08 23:02:42 +0200

    Added isMobile and customized clearScreen. Javadoc clean up.

--- kiwi/src/main/java/com/github/czyzby/kiwi/util/gdx/GdxUtilities.java
+++ kiwi/src/main/java/com/github/czyzby/kiwi/util/gdx/GdxUtilities.java
@@ -7,11 +7,12 @@ import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
+import com.github.czyzby.kiwi.util.common.UtilitiesClass;
 
 /** Provides generic utilities for pretty much any LibGDX application.
  *
  * @author MJ */
-public class GdxUtilities {
+public class GdxUtilities extends UtilitiesClass {
     private GdxUtilities() {
     }
 
@@ -21,12 +22,22 @@ public class GdxUtilities {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
     }
 
+    /** Clears the screen with the selected color.
+     *
+     * @param r red color value.
+     * @param g green color value.
+     * @param b blue color value. */
+    public static void clearScreen(final float r, final float g, final float b) {
+        Gdx.gl.glClearColor(r, g, b, 1f);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+    }
+
     /** Application's input processor will be set to null. */
     public static void clearInputProcessor() {
         Gdx.input.setInputProcessor(null);
     }
 
-    /** Application's input processor will be set to a multiplexer with multiple passed processors. */
+    /** @param processors application's input processor will be set to a multiplexer with multiple passed processors. */
     public static void setMultipleInputProcessors(final InputProcessor... processors) {
         Gdx.input.setInputProcessor(new InputMultiplexer(processors));
     }
@@ -36,7 +47,8 @@ public class GdxUtilities {
         return new Vector2(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
     }
 
-    /** @return the passed vector2 storing windows' width and height as x and y. */
+    /** @param result will be modified and returned.
+     * @return the passed vector2 storing windows' width and height as x and y. */
     public static Vector2 getScreenSize(final Vector2 result) {
         return result.set(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
     }
@@ -46,7 +58,8 @@ public class GdxUtilities {
         return new Vector2(Gdx.input.getX(), Gdx.input.getY());
     }
 
-    /** @return the passed vector2 storing current cursor position. */
+    /** @param result will be modified and returned.
+     * @return the passed vector2 storing current cursor position. */
     public static Vector2 getCursorPosition(final Vector2 result) {
         return result.set(Gdx.input.getX(), Gdx.input.getY());
     }
@@ -56,31 +69,43 @@ public class GdxUtilities {
         return new Vector3(Gdx.input.getAccelerometerX(), Gdx.input.getAccelerometerY(), Gdx.input.getAccelerometerZ());
     }
 
-    /** @return the passed vector3 storing current accelerometer data. */
+    /** @param result will be modified and returned.
+     * @return the passed vector3 storing current accelerometer data. */
     public static Vector3 getAccelerometerData(final Vector3 result) {
         return result.set(Gdx.input.getAccelerometerX(), Gdx.input.getAccelerometerY(), Gdx.input.getAccelerometerZ());
     }
 
+    /** @return true if application type equals {@link ApplicationType#Applet}. */
     public static boolean isRunningOnApplet() {
         return Gdx.app.getType() == ApplicationType.Applet;
     }
 
+    /** @return true if application type equals {@link ApplicationType#Android}. */
     public static boolean isRunningOnAndroid() {
         return Gdx.app.getType() == ApplicationType.Android;
     }
 
+    /** @return true if application type equals {@link ApplicationType#Android} or {@link ApplicationType#iOS}. */
+    public static boolean isMobile() {
+        return isRunningOnAndroid() || isRunningOnIOS();
+    }
+
+    /** @return true if application type equals {@link ApplicationType#Desktop}. */
     public static boolean isRunningOnDesktop() {
         return Gdx.app.getType() == ApplicationType.Desktop;
     }
 
+    /** @return true if application type equals {@link ApplicationType#iOS}. */
     public static boolean isRunningOnIOS() {
         return Gdx.app.getType() == ApplicationType.iOS;
     }
 
+    /** @return true if application type equals {@link ApplicationType#WebGL}. */
     public static boolean isRunningOnGwt() {
         return Gdx.app.getType() == ApplicationType.WebGL;
     }
 
+    /** @return true if application type equals {@link ApplicationType#HeadlessDesktop}. */
     public static boolean isHeadless() {
         return Gdx.app.getType() == ApplicationType.HeadlessDesktop;
     }
