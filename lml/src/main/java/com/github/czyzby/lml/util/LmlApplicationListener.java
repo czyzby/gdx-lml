2cea6b6fcd309c85f4dfe39ccf4ab4db2ab1d34d
MJ <john.hervicc@gmail.com>
2016-04-15 04:43:16 +0200

    Added DTD generation hook.

--- lml/src/main/java/com/github/czyzby/lml/util/LmlApplicationListener.java
+++ lml/src/main/java/com/github/czyzby/lml/util/LmlApplicationListener.java
@@ -1,6 +1,5 @@
 package com.github.czyzby.lml.util;
 
-import java.io.IOException;
 import java.io.Writer;
 
 import com.badlogic.gdx.ApplicationListener;
@@ -92,25 +91,43 @@ public abstract class LmlApplicationListener implements ApplicationListener {
     }
 
     /** Uses current {@link LmlParser} to generate a DTD schema file with all supported tags, macros and attributes.
-     * Should be used only during development: DTD allows to validate LML templates (and have content assign thanks to
-     * your IDE) during creation, but is not used by the {@link LmlParser} in runtime.
+     * Should be used only during development: DTD allows to validate LML templates during creation (and add content
+     * assist thanks to XML support in your IDE), but is not used in any way by the {@link LmlParser} in runtime.
      *
      * @param file path to the file where DTD schema should be saved. Advised to be local or absolute. Note that some
      *            platforms (GWT) do not support file saving - this method should be used on desktop platform and only
      *            during development.
-     * @throws GdxRuntimeException if unable to save DTD schema.
+     * @throws GdxRuntimeException when unable to save DTD schema.
      * @see Dtd */
     public void saveDtdSchema(final FileHandle file) {
         try {
             final Writer appendable = file.writer(false, "UTF-8");
             final boolean strict = lmlParser.isStrict();
             lmlParser.setStrict(false); // Temporary setting to non-strict to generate as much tags as possible.
-            Dtd.saveSchema(lmlParser, appendable);
+            createDtdSchema(lmlParser, appendable);
             appendable.close();
             lmlParser.setStrict(strict);
-        } catch (final IOException exception) {
+        } catch (final Exception exception) {
             throw new GdxRuntimeException("Unable to save DTD schema.", exception);
         }
+    }
+
+    /** This is a utility method that allows you to hook up into DTD generation process or even modify it completely.
+     * This method is called by {@link #saveDtdSchema(FileHandle)} after the parser was already set to non-strict. By
+     * default, this method calls standard DTD utility method: {@link Dtd#saveSchema(LmlParser, Appendable)}. By
+     * overriding this method, you can generate minified schema with
+     * {@link Dtd#saveMinifiedSchema(LmlParser, Appendable)} or manually append some customized tags and attributes
+     * using {@link Appendable} API.
+     *
+     * <p>
+     * If you want to generate DTD schema file for your LML parser, use {@link #saveDtdSchema(FileHandle)} method
+     * instead.
+     *
+     * @param parser its schema will be generated.
+     * @param appendable a reference to target file.
+     * @see #saveDtdSchema(FileHandle) */
+    protected void createDtdSchema(final LmlParser parser, final Appendable appendable) throws Exception {
+        Dtd.saveSchema(parser, appendable);
     }
 
     /** Called when application is created.
