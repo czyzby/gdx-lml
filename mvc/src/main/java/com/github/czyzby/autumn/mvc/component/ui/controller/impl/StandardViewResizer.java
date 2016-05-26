19430ea16bbdf1446aac3cae4c7e5857d6ee57d5
MJ <john.hervicc@gmail.com>
2016-05-26 17:33:06 +0200

    Updated to 1.7.1.9.3.

--- mvc/src/main/java/com/github/czyzby/autumn/mvc/component/ui/controller/impl/StandardViewResizer.java
+++ mvc/src/main/java/com/github/czyzby/autumn/mvc/component/ui/controller/impl/StandardViewResizer.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.github.czyzby.autumn.mvc.component.ui.controller.ViewResizer;
+import com.github.czyzby.kiwi.util.gdx.viewport.LetterboxingViewport;
 
 /** Resizes the screen by updating stage viewport.
  *
@@ -12,7 +13,7 @@ public class StandardViewResizer implements ViewResizer {
     @Override
     public void resize(final Stage stage, final int width, final int height) {
         final Viewport viewport = stage.getViewport();
-        final boolean centerCamera = viewport instanceof ScreenViewport;
+        final boolean centerCamera = viewport instanceof ScreenViewport || viewport instanceof LetterboxingViewport;
         viewport.update(width, height, centerCamera);
     }
 }
